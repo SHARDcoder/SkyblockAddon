4e0d290bfda241d378dfc090e53b21cae17fdcac
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2019-09-02 04:55:39 +0100

    just tweak stoof left

--- src/main/java/me/shardcoder/skyblockaddon/utils/Utils.java
+++ src/main/java/me/shardcoder/skyblockaddon/utils/Utils.java
@@ -4,6 +4,7 @@ import cc.hyperium.Hyperium;
 import cc.hyperium.event.ChatEvent;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.gui.main.HyperiumOverlay;
+import com.android.dx.rop.cst.CstArray;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
@@ -15,6 +16,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumMap;
@@ -95,8 +97,8 @@ public class Utils {
             if (sidebarObjective != null) {
                 onSkyblock = stripColor(sidebarObjective.getDisplayName()).startsWith("SKYBLOCK");
                 Collection<Score> collection = scoreboard.getSortedScores(sidebarObjective);
-                List<Score> list = List.newArrayList(collection.stream().filter(p_apply_1_ -> p_apply_1_.getPlayerName() != null && !p_apply_1_.getPlayerName().startsWith("#")).collect(
-                    Collections.toList()));
+                List<Score> list = new ArrayList<Object>(collection.stream().filter(p_apply_1_ -> p_apply_1_.getPlayerName() != null && !p_apply_1_.getPlayerName().startsWith("#")).collect(
+                    null));
                 if (list.size() > 15) {
                     collection = Lists.newArrayList(Iterables.skip(list, collection.size() - 15));
                 } else {
