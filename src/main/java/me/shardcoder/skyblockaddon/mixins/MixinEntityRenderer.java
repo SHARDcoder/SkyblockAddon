5c6d47341dd36d0f7c2200972a6639a35c8ed434
SHARDcoder <35032829+SHARDcoder@users.noreply.github.com>
2019-09-04 20:52:08 +0100

    it runs E

--- src/main/java/me/shardcoder/skyblockaddon/mixins/MixinEntityRenderer.java
+++ src/main/java/me/shardcoder/skyblockaddon/mixins/MixinEntityRenderer.java
@@ -1,24 +1,186 @@
 package me.shardcoder.skyblockaddon.mixins;
 
+import cc.hyperium.event.RenderWorldEvent;
+import cc.hyperium.handlers.handlers.reach.ReachDisplay;
+import cc.hyperium.mixinsimp.entity.HyperiumEntityRenderer;
+import com.google.common.base.Predicates;
 import java.util.List;
 import me.shardcoder.skyblockaddon.SkyblockAddon;
 import me.shardcoder.skyblockaddon.utils.Feature;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.item.EntityItemFrame;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EntitySelectors;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(EntityRenderer.class)
-public class MixinEntityRenderer {
+@Mixin(value = EntityRenderer.class, priority = 9001)
 
+
+
+public class MixinEntityRenderer {
+    //hyperium shid because priority
+    @Shadow
+    private float thirdPersonDistance;
+    @Shadow
+    private float thirdPersonDistanceTemp;
+    @Shadow
+    private boolean cloudFog;
+    @Shadow
+    private Minecraft mc;
+    @Shadow
+    private Entity pointedEntity;
+    private HyperiumEntityRenderer hyperiumEntityRenderer = new HyperiumEntityRenderer((EntityRenderer) (Object) this);
+    @Shadow
+    @Final
+    public static int shaderCount;
+
+    @Shadow
+    private int shaderIndex;
+
+    //endStartSection
+    @Inject(method = "updateCameraAndRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/profiler/Profiler;endStartSection(Ljava/lang/String;)V", shift = At.Shift.AFTER))
+    private void updateCameraAndRender(float partialTicks, long nano, CallbackInfo ci) {
+        hyperiumEntityRenderer.updateCameraAndRender();
+    }
+
+    @Inject(method = "activateNextShader", at = @At("INVOKE_ASSIGN"))
+    public void activateNextShader(CallbackInfo callbackInfo) {
+        HyperiumEntityRenderer.INSTANCE.isUsingShader = this.shaderIndex != shaderCount;
+    }
+
+    /**
+     * @author CoalOres
+     * @reason 360 Perspective
+     */
+    @Overwrite
+    private void orientCamera(float partialTicks) {
+        hyperiumEntityRenderer.orientCamera(partialTicks, this.thirdPersonDistanceTemp, this.thirdPersonDistance, this.cloudFog, this.mc);
+    }
+
+    @Inject(method = "renderWorldPass", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/profiler/Profiler;endStartSection(Ljava/lang/String;)V", args = "ldc=hand"))
+    private void onRenderWorld(int pass, float partialTicks, long nano, CallbackInfo info) {
+        new RenderWorldEvent(partialTicks).post();
+    }
+
+    @Inject(method = "renderWorldPass", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/profiler/Profiler;endStartSection(Ljava/lang/String;)V", args = "ldc=outline"), cancellable = true)
+    public void drawOutline(int pass, float part, long nano, CallbackInfo info) {
+        hyperiumEntityRenderer.drawOutline(part, this.mc);
+    }
+
+    @Inject(method = "updateCameraAndRender", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/profiler/Profiler;startSection(Ljava/lang/String;)V", args = "ldc=mouse"))
+    private void updateCameraAndRender2(float partialTicks, long nanoTime, CallbackInfo ci) {
+        hyperiumEntityRenderer.updatePerspectiveCamera();
+    }
+
+    /**
+     * @author - Sk1er (added forward for distance)
+     * @reason - ReachDisplay
+     */
+    @Overwrite
+    public void getMouseOver(float partialTicks) {
+        Entity entity = this.mc.getRenderViewEntity();
+        if (entity != null) {
+            if (this.mc.theWorld != null) {
+                this.mc.mcProfiler.startSection("pick");
+                this.mc.pointedEntity = null;
+                double d0 = (double) this.mc.playerController.getBlockReachDistance();
+                this.mc.objectMouseOver = entity.rayTrace(d0, partialTicks);
+                double d1 = d0;
+                Vec3 vec3 = entity.getPositionEyes(partialTicks);
+                boolean flag = false;
+
+                if (this.mc.playerController.extendedReach()) {
+                    d0 = 6.0D;
+                    d1 = 6.0D;
+                } else {
+                    if (d0 > 3.0D) {
+                        flag = true;
+                    }
+                }
+
+                if (this.mc.objectMouseOver != null) {
+                    d1 = this.mc.objectMouseOver.hitVec.distanceTo(vec3);
+                }
+
+                Vec3 vec31 = entity.getLook(partialTicks);
+                Vec3 vec32 = vec3.addVector(vec31.xCoord * d0, vec31.yCoord * d0, vec31.zCoord * d0);
+                this.pointedEntity = null;
+                Vec3 vec33 = null;
+                float f = 1.0F;
+                List<Entity> list = this.mc.theWorld.getEntitiesInAABBexcluding(entity, entity.getEntityBoundingBox().addCoord(vec31.xCoord * d0, vec31.yCoord * d0, vec31.zCoord * d0).expand((double) f, (double) f, (double) f), Predicates
+                    .and(EntitySelectors.NOT_SPECTATING, Entity::canBeCollidedWith));
+                double d2 = d1;
+
+                for (int j = 0; j < list.size(); ++j) {
+                    Entity entity1 = list.get(j);
+                    float f1 = entity1.getCollisionBorderSize();
+                    AxisAlignedBB axisalignedbb = entity1.getEntityBoundingBox().expand((double) f1, (double) f1, (double) f1);
+                    MovingObjectPosition movingobjectposition = axisalignedbb.calculateIntercept(vec3, vec32);
+
+                    if (axisalignedbb.isVecInside(vec3)) {
+                        if (d2 >= 0.0D) {
+                            this.pointedEntity = entity1;
+                            vec33 = movingobjectposition == null ? vec3 : movingobjectposition.hitVec;
+                            d2 = 0.0D;
+                        }
+                    } else if (movingobjectposition != null) {
+                        double d3 = vec3.distanceTo(movingobjectposition.hitVec);
+
+                        if (d3 < d2 || d2 == 0.0D) {
+                            if (entity1 == entity.ridingEntity) {
+                                if (d2 == 0.0D) {
+                                    this.pointedEntity = entity1;
+                                    vec33 = movingobjectposition.hitVec;
+                                }
+                            } else {
+                                this.pointedEntity = entity1;
+                                vec33 = movingobjectposition.hitVec;
+                                d2 = d3;
+                            }
+                        }
+                    }
+                }
+                double v = 0;
+
+                if (this.pointedEntity != null && flag && (v = vec3.distanceTo(vec33)) > 3.0D) {
+                    this.pointedEntity = null;
+                    this.mc.objectMouseOver = new MovingObjectPosition(MovingObjectPosition.MovingObjectType.MISS, vec33, null, new BlockPos(vec33));
+                }
+                if (v != 0 || this.pointedEntity != null)
+                    ReachDisplay.dis = v;
+
+
+                if (this.pointedEntity != null && (d2 < d1 || this.mc.objectMouseOver == null)) {
+                    this.mc.objectMouseOver = new MovingObjectPosition(this.pointedEntity, vec33);
+
+                    if (this.pointedEntity instanceof EntityLivingBase || this.pointedEntity instanceof EntityItemFrame) {
+                        this.mc.pointedEntity = this.pointedEntity;
+                    }
+                }
+
+                this.mc.mcProfiler.endSection();
+            }
+        }
+    }
+
+
+    //skyblock shid
     @Inject(method = "getMouseOver", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void getMouseOver(float partialTicks, CallbackInfo ci, Entity entity, double d0, double d1, Vec3 vec3, boolean flag, boolean b, Vec3 vec31, Vec3 vec32, Vec3 vec33, float f, List<Entity> list, double d2, int j) {
         removeEntities(list);
